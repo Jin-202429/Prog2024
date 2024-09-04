b174e622af4168386b7fc5773e36be6064d43bb1
Jin-202429 <ncheeunho@gmail.com>
2024-09-04 14:31:19 +0900

    2주차 수업 내용

---
+++ 009_sizeof/009_sizeof.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("sizeof char = %d\n", sizeof(char));
+	printf("sizeof short = %d\n", sizeof(short));
+	printf("sizeof long = %d\n", sizeof(long));
+	printf("sizeof int = %d\n", sizeof(unsignint));
+	printf("sizeof long long = %d\n", sizeof(long long));
+
+	printf("sizeof ")
+
+}
---
+++ 011_average/011_average.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	int math = 95;
+	int eng = 97;
+	int kor = 88;
+
+	int sum = math + eng + kor; 
+	double avg = (double)sum / 3;  // 정수 / 정수 -> 정수      // double avg = sum / 3 => 93.000000으로 결과값이 나온다   // double avg = (double)sum / 3 => 93.333333으로 결과값이 나온다  
+	                               // 이렇게 결과값이 나오는 이유는 sum이 정수형 변수이기 때문이다
+
+	printf("평균 : %f\n", avg);
+}
+
